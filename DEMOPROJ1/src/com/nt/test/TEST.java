8510a5519b09ad61ccd7bdd27c0e436db2faf730
RPAVAN <RPAVAN@pavan>
2021-03-22 14:08:16 +0530

    dev1 moodified test1.java

--- DEMOPROJ1/src/com/nt/test/TEST.java
+++ DEMOPROJ1/src/com/nt/test/TEST.java
@@ -1,5 +1,9 @@
 package com.nt.test;
-
+import com.nt.bean.UTILITY;
 public class TEST {
-
+ public static void main(String [] agrs) {
+	 UTILITY ut= new UTILITY();
+System.out.println(ut.getSysDate());
+ System.out.println("writtern by dev 1");
+}
 }
