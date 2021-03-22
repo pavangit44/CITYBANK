2c3f214bdb62a6eab868cd4a184bfd3130853f1c
RPAVAN <RPAVAN@pavan>
2021-03-22 15:55:22 +0530

    utility.java

--- DEMOPROJ1/src/com/nt/bean/UTILITY.java
+++ DEMOPROJ1/src/com/nt/bean/UTILITY.java
@@ -1,15 +1,13 @@
 package com.nt.bean;
 import java.util.Date;
 public class UTILITY {
+	
 
 	public Date getSysDate() {
 		return new Date();
 	}
 	public void m1() {
 	System.out.println("utility m1 method:1234");
-
-
-	System.out.println("modifiedadnb aa");
-	System.out.println("modified1234");
 	}
 }
+
