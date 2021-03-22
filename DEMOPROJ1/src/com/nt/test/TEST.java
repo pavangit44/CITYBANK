34d6aa9ec337b0fdeae4885c42867089e56a8798
RPAVAN <RPAVAN@pavan>
2021-03-22 14:23:46 +0530

    test.java deleted by h"TL"

--- DEMOPROJ1/src/com/nt/test/TEST.java
+++
@@ -1,9 +0,0 @@
-package com.nt.test;
-import com.nt.bean.UTILITY;
-public class TEST {
- public static void main(String [] agrs) {
-	 UTILITY ut= new UTILITY();
-System.out.println(ut.getSysDate());
- System.out.println("writtern by dev 1");
-}
-}
